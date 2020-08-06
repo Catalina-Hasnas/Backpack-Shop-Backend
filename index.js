3e12ba6ab6e3daa1776205b3b475e8f183db4c30
Catalina Hasnas <catalinahasnas1996@gmail.com>
2020-08-06 10:52:00 +0300

    created orders

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/products/:id', (req, res) => {
-  let order = db.get("products").find(order => order.id == req.params.id);
-  if(order){
-    res.send(order);
+  let product = db.get("products").find(product => product.id == req.params.id);
+  if(product){
+    res.send(product);
   }
   res.send({error: true, message: "Product with id: " + req.params.id + " not found"});
 });
@@ -45,7 +45,7 @@ app.get('/products/:id', (req, res) => {
 app.post('/products/new', (req, res) => {
   // Content-Type: application/json; charset=utf-8
   // console.log(req.body);
-  let order = {
+  let product = {
     "id": uuid.v4(),
     "name": req.body.name,
     "img": req.body.img,
@@ -57,10 +57,55 @@ app.post('/products/new', (req, res) => {
     "category": req.body.category,
     "type": req.body.type
   }
-  db.set('products', order);
+  db.set('products', product);
+  res.status(200).json(product);
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+app.get('/orders', (req,res) => {
+  res.send(db.get("orders"));
+});
+
+app.get('/orders/:id', (req, res) => {
+  let order = db.get("orders").find(order => order.id == req.params.id);
+  if(order){
+    res.send(order);
+  }
+  res.send({error: true, message: "Order with id: " + req.params.id + " not found"});
+});
+
+app.post('/orders/new', (req, res) => {
+  let order = {
+    "id" : uuid.v4(),
+    "products" : []
+  };
+  db.set('orders', order);
   res.status(200).json(order);
 });
 
+app.post('/orders/addProduct', (req, res) => {
+  let order = db.get("orders").find(order => order.id == req.body.orderId);
+
+  if(order){
+    db.pop('orders', order);
+    productIndex = order.products.findIndex(product => product.productId == req.body.productId);
+    if(productIndex > -1) {
+      order.products[productIndex] = {
+        "productId" : req.body.productId,
+        "quantity" : req.body.quantity
+      };
+    } else {
+      order.products.push({
+        "productId" : req.body.productId,
+        "quantity" : req.body.quantity
+      });
+    }
+    db.set('orders', order);
+    res.status(200).json(order);
+  }
+  res.send({error: true, message: "Order with id: " + req.body.id + " not found"});
+});
 
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
